37289fbc629e6eba47d6dc681eda9704435ebeb6
annassiddique <annassiddique7500@gmail.com>
2024-11-21 16:35:39 +0500

    initial setup using vite/springboot

---
+++ backend/src/main/java/com/nyx/lindkedIn/LindkedInApplication.java
@@ -0,0 +1,13 @@
+package com.nyx.lindkedIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindkedInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindkedInApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nyx/lindkedIn/LindkedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyx.lindkedIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindkedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
